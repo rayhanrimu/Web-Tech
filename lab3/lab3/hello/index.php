e840a6c56e7908789b3b2548b0ae6aa9b47eb3d4
Rayhan Rimu <rayhanrimu87@gmail.com>
2018-07-09 04:11:11 +0600

    Lab3
    Lab 3 all work is here

---
+++ lab3/lab3/hello/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Hello World!</h1>";
+echo "<img src='image/argentina-flag1.jpg' height='100px' width='200px' alt='image'/>";
+?>
---
+++ lab3/lab3/rimu/something.php
@@ -0,0 +1,17 @@
+<?php
+
+for($x=0; $x<=9;$x++)
+{
+	echo "Name: Rimu <br>" ;
+}
+for($x=1; $x<=10;$x++)
+{
+	echo "$x. Name: Rimu <br>" ;
+}
+$car=array("one","two","three","four","five","six","seven","eight","nine","ten");
+for($x=0; $x<=9;$x++)
+{
+	echo "$car[$x]. Name: Rimu <br>" ;
+}
+
+?>
